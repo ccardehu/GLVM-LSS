a53f96d8e36bc0ca24427c649e624b64b5957c31
Camilo Cardenas <c.a.cardenas-hurtado@lse.ac.uk>
2021-01-25 17:23:29 +0000

    Update (graphFun.R): Changes in plot functions

--- graphFun.R
+++ graphFun.R
@@ -178,7 +178,7 @@ num.score <- grad(func = loglik, x = unlist(mod$b), method = "Richardson",
 tmp.min <- min(num.score,unlist(mod$Score)) - 1e-5
 tmp.max <- max(num.score,unlist(mod$Score)) + 1e-5
 plot.ts(unlist(mod$Score), pch = 16, col = "gray40", lwd = 2, main = "Analytical (in EM) vs. Numerical Scores",
-        ylim = c(tmp.min,tmp.max), xlab = "Paramter (#)", ylab = "Score value"); abline(h = 0, col = "blue", lty = 2)
+        ylim = c(tmp.min,tmp.max), xlab = "Parameters (#)", ylab = "Score value"); abline(h = 0, col = "blue", lty = 2)
 lines(num.score, col = 2, lwd = 2)
 legend("bottomright",legend=c("Analytical", "Numerical"), col=c("black", "red"), cex=1, lty = 1, inset = 0.02, bty = "n")
 }
